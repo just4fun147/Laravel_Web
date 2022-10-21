155c8c6ea546921ecbc90aeb1f3740a0af213215
just4fun147 <pauluswindito1@gmail.com>
2022-10-21 12:17:24 +0700

    Dream Car

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,53 @@
         <p class="second"><b>WELCOME TO MY WEBSITE</b></p>
         <p class="third">This website is developed with laravel, bootstrap, php, html, css, and javascript.</p>
         <p class="third">By <b>Paulus Pandu Windito-2022</b></p>
+        <p><i class="arrow down"></i></p>
     </div>
 
     <div class="right">
         <p class="secondR"><b>PROGRAMMER</b></p>
         <p class="thirdR"><b>WEB</b></p>
         <p class="thirdR"><b>DEVELOPER_</b></p>
+        
     </div>
+
+<section>
+    <div>
+        <div class="rectangle"></div>
+            <div class="elem">
+              <div class="text-block">
+                <p class="block">aaaaaa</p>
+                <p class="block"></p>
+                <h2 class="mb-0 mb-01">My Dream Car</h2>
+                <h5 class="mb-0">Hyundai Ioniq 5</h5>
+                <p class="mb-0">the first vehicle built on Hyundai’s new Electric-Global Modular Platform (E-GMP). This dedicated battery electric vehicle platform enables faster charging, increased driving range, more interior space, and better handling. The battery pack’s position within the wheelbase ensures ideal weight distribution between the front and rear and a low center of gravity for improved handling performance.</p>
+                <a class="mb-0" href="https://www.hyundai.com/worldwide/en/eco/ioniq5/highlights">More...</a>
+                <p class="block"></p>
+                <p class="block"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="data" class="section">
+        <div class="row no-gutters">
+          <div class="col">
+            <div class="elem">
+              <div>
+                <img
+                  src="/img/ioniq5.jpg"
+                  class="img-fluid"
+                  alt="Responsive image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</section>
+    
+    
+      </section>
 @endsection
 
 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,20 +2,20 @@
     <div class="container">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" {{ ($title === "Home") ? 'active' : '' }} href="/">Pandu</a>
+                <a class="nav-link" {{ ($title === "Home") ? 'active' : '' }} href="/">Pandu_</a>
             </li>
         </ul>
     </div>
     <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" {{ ($title === "Home") ? 'active' : '' }} href="/">Home</a>
+                <a class="nav-link" {{ ($title === "Home") ? 'active' : '' }} href="/">Home_</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" {{ ($title === "About") ? 'active' : '' }} href="/about">About</a>
+                <a class="nav-link" {{ ($title === "About") ? 'active' : '' }} href="/about">About_</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" {{ ($title === "Feedback") ? 'active' : '' }} href="/feedback">Feedback</a>
+                <a class="nav-link" {{ ($title === "Feedback") ? 'active' : '' }} href="/feedback">Feedback_</a>
             </li>
         </ul>
     </div>
